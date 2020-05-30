ce8f2cd79b27dfe5edcd8c3efc9f5a41a8dc79f4
FRIEND-PC <FRIEND-PC@DESKTOP-82CCF2U>
2020-05-30 14:44:50 +0700

    commit code

---
+++ LiteDB/Adventure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteDB
+{
+    class Adventure
+    {
+        public int id { get; set; }
+        public string activityName { get; set; }
+        public DateTime activityDate { get; set; }
+        public string address { get; set; }
+        public decimal lon { get; set; }
+        public decimal lat { get; set; }
+    }
+}
